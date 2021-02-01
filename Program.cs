8cc6199528f80dda78dac35fcc2b246d95b99f49
Jordan Fischer <jordan@thefischers.org>
2021-02-01 18:23:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace D3_Methods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your name:");
+			PrintWelcome(Console.ReadLine());
+		}
+
+		private static void PrintWelcome(string name)
+		{
+			Console.WriteLine($"Hello {name}!");
+		}
+	}
+}
